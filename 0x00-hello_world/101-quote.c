b853dc2c7d61680f29e39b84e89d83bc68ca79ea
bwissal <baazizwissal@gmail.com>
2023-03-16 07:59:24 -0700

    c programme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
-#include <string.h>
-
-int main()
+/**
+ *main-program entry point.
+ *
+ *Return: 1 on success.
+ */
+int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen(message));
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
