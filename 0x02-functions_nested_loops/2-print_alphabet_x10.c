c848b7687e59daa4850bca2f9ca481669a158f8a
bwissal <baazizwissal@gmail.com>
2023-03-21 15:21:41 -0700

    all

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "1-alphabet.c"
+/**
+ * print_alphabet_x10 - Prints alphabets in lower case
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		print_alphabet();
+		n++;
+	}
+}
