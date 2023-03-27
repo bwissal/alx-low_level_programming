3238a040857543e228eb61a8bde75603c9425d56
bwissal <baazizwissal@gmail.com>
2023-03-27 15:09:17 -0700

    all

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - W
+ * @a: A
+ * @b: B
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory conatain 0x05-pointers_arrays_strings project
